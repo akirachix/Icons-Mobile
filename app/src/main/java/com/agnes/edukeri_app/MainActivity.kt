46c0f2637ad76e63a6660d992d3b8838e4f0efd6
Aber-Racheal <rachealaberr@gmail.com>
2024-07-16 22:25:49 +0300

    add advanced digital skills

--- app/src/main/java/com/agnes/edukeri_app/MainActivity.kt
+++ app/src/main/java/com/agnes/edukeri_app/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.agnes.edukeri_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvAdvancedLevels.layoutManager = LinearLayoutManager(this)
+
+        displayItems()
+
         }
-    }
-}
+
+
+    fun displayItems(){
+
+        val level1= Advancedlevel(R.drawable.img_1, "Advanced", R.drawable.circle)
+        val level2= Advancedlevel(R.drawable.img, "Advanced", R.drawable.circle)
+        val level3 = Advancedlevel(R.drawable.img_2, "Advanced", R.drawable.circle)
+
+        val advancedList = listOf(level1, level2, level3)
+        val advancedAdapter = AdvancedAdapter(advancedList)
+        binding.rvAdvancedLevels.adapter = advancedAdapter
+    }}
